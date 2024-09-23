b98f6f89b14b198891083b5d8c697fedbdf08e74
rezwan444 <rezwanulhaqunayem@gmail.com>
2024-09-24 00:42:36 +0600

    index file add

---
+++ JS/common-function.js
@@ -0,0 +1,4 @@
+function getInputFieldValueById(){
+   const donateNow = document.getElementById('btn-donate-now').value;
+   return donateNow;
+}
---
+++ JS/donation-amount.js
@@ -0,0 +1,7 @@
+const mainBalance = 100000;
+
+let donation1 = 0;
+let donation2 = 0;
+let donation3 = 0;
+
+function
